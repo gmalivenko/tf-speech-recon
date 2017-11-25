bf6aa887d5bceedf5face8b3fc7e6040f531b0ce
Vitaly <square.nabla@gmail.com>
2017-11-25 15:34:26 +0100

    test lace with random data

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+
 from feature_extractor import *
+from ann import *
 
-fe = FeatureExtractor("./data/train/audio/one/", ",")
 
-print(fe.sample(10))
+import numpy as np
+import tensorflow as tf
+
+
+# fe = FeatureExtractor("./data/train/audio/one/", ",")
+
+# print(fe.sample(10))
 # fe.visualize()
+data = np.random.rand(100, 61, 40, 1)
+labels = np.random.randint(10, size=100)
+
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+
+    network = ANN(sess)
+    network.build_lace(10, input_size=[61, 40, 1])
+    # sess.run(init)
+
+    network.train(data, labels, 0.0001)
