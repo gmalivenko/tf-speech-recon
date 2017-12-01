ee4609116b10e4d547bbc139af280b3e1b9b3e9a
Vitaly <square.nabla@gmail.com>
2017-12-01 16:52:41 +0100

    test net

---
+++ speech_commands/runNet.py
@@ -0,0 +1,210 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os.path
+import sys
+import csv
+
+import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+
+import input_data
+import submission_processor
+import models
+from tensorflow.python.platform import gfile
+
+FLAGS = None
+
+
+def load_labels(filename):
+  """Read in labels, one label per line."""
+  return [line.rstrip() for line in tf.gfile.GFile(filename)]
+
+
+def main(_):
+  # We want to see all the logging messages for this tutorial.
+  tf.logging.set_verbosity(tf.logging.INFO)
+
+  # Start a new TensorFlow session.
+  sess = tf.InteractiveSession()
+
+  model_settings = models.prepare_model_settings(
+      len(input_data.prepare_words_list(FLAGS.wanted_words.split(','))),
+      FLAGS.sample_rate, FLAGS.clip_duration_ms, FLAGS.window_size_ms,
+      FLAGS.window_stride_ms, FLAGS.dct_coefficient_count)
+  audio_processor = submission_processor.SubmissionProcessor(model_settings)
+  fingerprint_size = model_settings['fingerprint_size']
+
+  fingerprint_input = tf.placeholder(
+      tf.float32, [None, fingerprint_size], name='fingerprint_input')
+
+  logits = models.create_model(fingerprint_input, model_settings, FLAGS.model_architecture, is_training=False)
+
+  predicted_indices = tf.argmax(logits, 1)
+
+  #tf.global_variables_initializer().run()
+
+  models.load_variables_from_checkpoint(sess, FLAGS.start_checkpoint)
+
+  indices = []
+  for i in xrange(0, 158538, FLAGS.batch_size):
+    test_fingerprints = audio_processor.get_test_data(FLAGS.batch_size, i, model_settings)
+    #print (test_fingerprints)
+    batch_indices = sess.run(predicted_indices, feed_dict={fingerprint_input: test_fingerprints})
+    indices.extend(batch_indices)
+  path_to_labels = FLAGS.labels
+  labels = load_labels(path_to_labels)
+  human_string = []
+  for i in indices:
+      human_string.append(labels[i])
+  audio_processor.write_to_csv(human_string)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--labels', type=str, default='/tmp/speech_commands_train/lace_labels.txt', help='Path to file containing labels.')
+  parser.add_argument(
+      '--data_url',
+      type=str,
+      # pylint: disable=line-too-long
+      default='http://download.tensorflow.org/data/speech_commands_v0.01.tar.gz',
+      # pylint: enable=line-too-long
+      help='Location of speech training data archive on the web.')
+  parser.add_argument(
+      '--data_dir',
+      type=str,
+      default='./data/train/audio/',
+      help="""\
+      Where to download the speech training data to.
+      """)
+  parser.add_argument(
+      '--background_volume',
+      type=float,
+      default=0.1,
+      help="""\
+      How loud the background noise should be, between 0 and 1.
+      """)
+  parser.add_argument(
+      '--background_frequency',
+      type=float,
+      default=0.8,
+      help="""\
+      How many of the training samples have background noise mixed in.
+      """)
+  parser.add_argument(
+      '--silence_percentage',
+      type=float,
+      default=10.0,
+      help="""\
+      How much of the training data should be silence.
+      """)
+  parser.add_argument(
+      '--unknown_percentage',
+      type=float,
+      default=10.0,
+      help="""\
+      How much of the training data should be unknown words.
+      """)
+  parser.add_argument(
+      '--time_shift_ms',
+      type=float,
+      default=100.0,
+      help="""\
+      Range to randomly shift the training audio by in time.
+      """)
+  parser.add_argument(
+      '--testing_percentage',
+      type=int,
+      default=10,
+      help='What percentage of wavs to use as a test set.')
+  parser.add_argument(
+      '--validation_percentage',
+      type=int,
+      default=10,
+      help='What percentage of wavs to use as a validation set.')
+  parser.add_argument(
+      '--sample_rate',
+      type=int,
+      default=16000,
+      help='Expected sample rate of the wavs',)
+  parser.add_argument(
+      '--clip_duration_ms',
+      type=int,
+      default=1000,
+      help='Expected duration in milliseconds of the wavs',)
+  parser.add_argument(
+      '--window_size_ms',
+      type=float,
+      default=30.0,
+      help='How long each spectrogram timeslice is',)
+  parser.add_argument(
+      '--window_stride_ms',
+      type=float,
+      default=10.0,
+      help='How long each spectrogram timeslice is',)
+  parser.add_argument(
+      '--dct_coefficient_count',
+      type=int,
+      default=40,
+      help='How many bins to use for the MFCC fingerprint',)
+  parser.add_argument(
+      '--how_many_training_steps',
+      type=str,
+      default='15000,3000',
+      help='How many training loops to run',)
+  parser.add_argument(
+      '--eval_step_interval',
+      type=int,
+      default=400,
+      help='How often to evaluate the training results.')
+  parser.add_argument(
+      '--learning_rate',
+      type=str,
+      default='0.001,0.0001',
+      help='How large a learning rate to use when training.')
+  parser.add_argument(
+      '--batch_size',
+      type=int,
+      default=100,
+      help='How many items to train with at once',)
+  parser.add_argument(
+      '--summaries_dir',
+      type=str,
+      default='/tmp/retrain_logs',
+      help='Where to save summary logs for TensorBoard.')
+  parser.add_argument(
+      '--wanted_words',
+      type=str,
+      default='yes,no,up,down,left,right,on,off,stop,go',
+      help='Words to use (others will be added to an unknown label)',)
+  parser.add_argument(
+      '--train_dir',
+      type=str,
+      default='/tmp/speech_commands_train',
+      help='Directory to write event logs and checkpoint.')
+  parser.add_argument(
+      '--save_step_interval',
+      type=int,
+      default=100,
+      help='Save model checkpoint every save_steps.')
+  parser.add_argument(
+      '--start_checkpoint',
+      type=str,
+      default='/tmp/speech_commands_train/lace.ckpt-18000',
+      help='If specified, restore this pretrained model before any training.')
+  parser.add_argument(
+      '--model_architecture',
+      type=str,
+      default='lace',
+      help='What model architecture to use')
+  parser.add_argument(
+      '--check_nans',
+      type=bool,
+      default=False,
+      help='Whether to check for invalid numbers during processing')
+
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
