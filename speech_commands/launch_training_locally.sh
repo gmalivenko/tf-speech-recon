ba855a6fc037727848a83124f58600061c6465e8
Vitaly <square.nabla@gmail.com>
2017-12-08 15:17:02 +0100

    config file for changing paths and arch params

---
+++ speech_commands/launch_training_locally.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+TF_PY="/home/vitaly/new_tf/bin/python3"
+
+TRAIN_DATA_DIR="/home/vitaly/PycharmProjects/tf-speech-recon/data/train/audio/"
+TEST_DATA_DIR="/home/vitaly/PycharmProjects/tf-speech-recon/data/test/audio/"
+
+CHECKPOINT="/home/vitaly/competition/graph/lace_32/lace.ckpt-18000"
+
+MODEL_CONFIG="model_configs/lace_config"
+
+$TF_PY train.py --data_dir=$TRAIN_DATA_DIR --start_checkpoint=$CHECKPOINT --arch_config_file=$MODEL_CONFIG
